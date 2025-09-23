bbb918bc8e036f8e8737264fda8760dcf14f9fdb
unknown <24020136@vnu.edu.vn>
2025-09-23 10:41:34 +0700

    HW1 commit

---
+++ src/Introduction.java
@@ -0,0 +1,19 @@
+public class Introduction {
+    public static void main(String[] args) {
+        // In thông tin cá nhân (chỉnh sửa lại cho đúng của bạn)
+        System.out.println("Bui Huy Hoang\t24020136\tINT22041\thuyhoantbhh\t24020136@vnu.edu.vn");
+
+        // In bài hát bottles of beer
+        for (int i = 9; i >= 1; i--) {
+            if (i > 1) {
+                System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+                System.out.println("Take one down, pass it around,");
+            } else {
+                // Trường hợp còn 1 chai
+                System.out.println(i + " bottle of beer on the wall, " + i + " bottle of beer.");
+                System.out.println("Take one down, pass it around,");
+                System.out.println("No more bottles of beer on the wall.");
+            }
+        }
+    }
+}
